f1c0acc2b113144369fd7eb5795f5fcc96ab3a2f
Juan Suarez <jp.juansuarez@gmail.com>
2022-12-21 14:33:13 -0300

    Added ejercicio 1

---
+++ Ejercicio1/script.js
@@ -0,0 +1,22 @@
+/* 
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+- Tu nombre (string)
+
+- Tu edad (number)
+
+- ¿Eres desarrollador? (boolean)
+
+- Tu fecha de nacimiento (Date)
+
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) 
+*/
+
+const listaDatos = [Juan, 25, False, "10/19/1996"]
+
+const libroFav = {
+    titulo: 1986,
+    autor: "George Orwell",
+    fechaPublicacion: "6/8/1949",
+    url: "https://es.wikipedia.org/wiki/1984_(novela)"
+};
